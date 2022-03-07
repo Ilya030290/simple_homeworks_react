d11622decaa58dbca4bddd1567c9c71332583732
Ilya030290 <anoshko90@mail.ru>
2022-03-07 21:13:58 +0300

    HW5 refactoring completed

--- src/p2-homeworks/h5/Routers.tsx
+++ src/p2-homeworks/h5/Routers.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Route, Routes} from "react-router-dom";
+import {Route, Routes, Navigate} from "react-router-dom";
 import Error404 from "./pages/Error404";
 import PreJunior from "./pages/PreJunior";
 import Junior from "./pages/Junior";
@@ -20,11 +20,12 @@ function Routers() {
             <Routes>
                 {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу PRE_JUNIOR*/}
                 {/*exact нужен чтоб указать полное совподение (что после '/' ничего не будет)*/}
-                <Route path={'/*'} element={<PreJunior/>}/>
+                <Route path={'/'} element={<Navigate to={PATH.PRE_JUNIOR}/>}/>
+                <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>
                 <Route path={PATH.JUNIOR} element={<Junior/>}/>
                 <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus/>}/>
                 {/*у этого роута нет пути, он отрисуется если пользователь захочет попасть на несуществующую страницу*/}
-                <Route element={<Error404/>}/>
+                <Route path={'/*'} element={<Error404/>}/>
             </Routes>
         </div>
     )
--- src/p2-homeworks/h5/pages/Error404.tsx
+++ src/p2-homeworks/h5/pages/Error404.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import s from '../Header.module.css'
 
 function Error404() {
     return (
-        <div>
-            <div>404</div>
+        <div className={s.error}>
+            <div>Error 404...</div>
             <div>Page not found!</div>
-            <div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
+            <div>—ฅ/ᐠ.̫ .ᐟ\ฅ— </div>
         </div>
     )
 }
